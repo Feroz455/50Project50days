03d3bf283ff4b118fe10bb31ff186979b4c87cbd
Feroz455 <Fmahmud455@gmail.com>
2022-11-12 10:29:59 +0600

    Project 14 Amimated Navigation uploaded

---
+++ 14_Animated_Navigation/script.js
@@ -0,0 +1,5 @@
+const toggle = document.getElementById("toggle");
+const nav = document.getElementById("nav");
+toggle.addEventListener("click", function () {
+  nav.classList.toggle("active");
+});
