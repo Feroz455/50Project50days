b76b29a9722cd9c094c44ba90592a95f127e17b4
Feroz455 <Fmahmud455@gmail.com>
2022-11-12 09:47:57 +0600

    Project 13 Random choice Picker uploaded

--- 13_Random_Choice_Picker/script.js
+++ 13_Random_Choice_Picker/script.js
@@ -1,10 +1,19 @@
 const tagsEl = document.getElementById("tags");
 const textarea = document.getElementById("textarea");
 textarea.focus();
-textarea.addEventListener("keyup", createTags);
+textarea.addEventListener("keyup", (e) => {
+  createTags(e.target.value);
+  if (e.key == "Enter") {
+    setTimeout(() => {
+      e.target.value = "";
+    }, 10);
+    randomSelect();
+  }
+});
+tagsEl.innerHTML = "";
 
 function createTags(input) {
-  const tags = input.target.value
+  const tags = input
     .split(",")
     .filter((tag) => tag.trim() !== "")
     .map((tag) => tag.trim());
@@ -17,4 +26,32 @@ function createTags(input) {
     tagEl.innerText = tag;
     tagsEl.appendChild(tagEl);
   });
+}
+function randomSelect() {
+  const times = 30;
+  const interval = setInterval(() => {
+    const randomTag = PickRandomTag();
+    heightLightTag(randomTag);
+    setTimeout(() => {
+      unheightLightTag(randomTag);
+    }, 100);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+    setTimeout(() => {
+      const randomTag = PickRandomTag();
+      heightLightTag(randomTag);
+    }, 100);
+  }, times * 100);
+}
+function PickRandomTag() {
+  const tags = document.querySelectorAll(".tag");
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+function heightLightTag(tag) {
+  tag.classList.add("highlight");
+}
+function unheightLightTag(tag) {
+  tag.classList.remove("highlight");
 }
